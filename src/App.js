b2ba8fce7666b30d72ca5ed039ae0827d10801f6
Haobo Wang <wang2940@purdue.edu>
2021-11-10 18:12:45 -0500

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Papa from 'papaparse';
+import VirtualScroller from "./VirtualScroller";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SETTINGS = {
+  itemHeight: 20,
+  amount: 35,
+  tolerance: 5,
+  minIndex: 1,
+  maxIndex: 2000,
+  startIndex: 1,
+};
+
+const getData = (offset, limit, rows) => {
+
+  const start = Math.max(SETTINGS.minIndex, offset)
+  const end = Math.min(offset + limit - 1, SETTINGS.maxIndex)
+  const data = (start <= end) ? rows.slice(start, end + 1) : rows
+  return data
 }
 
-export default App;
+export default function App() {
+  const [rows, setRows] = React.useState([]);
+  React.useEffect(async () => {
+    await Papa.parse("/acho_export_virtual_scroll.csv", {
+      download: true,
+      header: true,
+      complete: data => {
+        // console.log(data.meta['fields'])
+        setRows(data.data);
+        // update data length
+        SETTINGS.maxIndex = data.data.length;
+      }
+    });
+  }, []);
+
+  const columns = [];
+  const colLen = 1107;
+  for(let i = 1; i <= colLen; i++){
+    columns.push(`field_${i}`);
+  }
+
+  const rowTemplate = item => (
+    <tr className="item_row">
+      {
+        Object.entries(item).map(([key, value], index) => (
+          <td className="item_col" key={index}>{value}</td>
+        ))
+      }
+
+    </tr>
+    // <tr className="item_row" key={item.index}>
+    //   {
+    //     item.text.map((t, i) => (
+    //       <td className="item_col" key={i}>{t}</td>
+    //     ))
+    //   }
+    // </tr>
+  )
+
+  return (
+    <div className="App">
+      <h1>
+        <center>React Table Demo</center>
+      </h1>
+      <VirtualScroller
+        className="viewport"
+        get={getData}
+        settings={SETTINGS}
+        row={rowTemplate}
+        rows={rows}
+        columns={columns}
+      />
+    </div>
+  )
+};
